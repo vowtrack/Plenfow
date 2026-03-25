dcc1d5ba7b27c2bff7ac0b4d86cf4f18da25956d
vowtrack <prayogoibrahim@gmail.com>
2026-03-25 14:37:20 +0700

    Delete utils.ts

--- utils.ts
+++
@@ -1,8 +0,0 @@
-// Helper untuk menggabungkan class Tailwind dengan aman
-// Contoh: cn("px-4", condition && "bg-red-500") 
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
